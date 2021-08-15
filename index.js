951143811b22eae2521be00f1ee6a6c3c25a2093
DayDarkTV <dayofdarkness71@gmail.com>
2021-08-15 12:18:18 -0400

    Fix Content Not Displaying Properly On Smaller Screens

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 //     const Twitch = require()
 // }
 new Twitch.Embed("twitch-embed", {
-    width: 854,
-    height: 480,
+    width: "100%",
     channel: "daydarktv",
     layout: "video",
     parent: ["daydarktv.github.io"]
