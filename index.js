a25039652c7d463dba0df5fe3c87510c7776168e
DayDarkTV <dayofdarkness71@gmail.com>
2021-08-15 11:58:17 -0400

    Testing Twitch Embed

---
+++ index.js
@@ -0,0 +1,9 @@
+// if (typeof require === 'function') {
+//     const Twitch = require()
+// }
+new Twitch.Embed("twitch-embed", {
+    width: 854,
+    height: 480,
+    channel: "daydarktv",
+    parent: ["daydarktv.github.io"]
+});
